caa5421bc5cbbce0bd6ce123333ceb938cf1cc07
Kevin <kevin@kpturner.co.uk>
2022-11-01 15:08:07 +0000

    Add new parm to config

--- src/config.js
+++ src/config.js
@@ -15,6 +15,7 @@ class Config {
       iamRoleName: core.getInput('iam-role-name'),
       runnerHomeDir: core.getInput('runner-home-dir'),
       preRunnerScript: core.getInput('pre-runner-script'),
+      runAsService: core.getInput('run-runner-as-service')
     };
 
     const tags = JSON.parse(core.getInput('aws-resource-tags'));
